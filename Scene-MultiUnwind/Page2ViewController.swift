907a7816364cacf118f6debd4223b2d242863033
윤성호 <youn2029@naver.com>
2019-01-14 16:44:39 +0900

    Unwind segue를 이용해 중간 페이지로 복귀

---
+++ Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 윤성호 on 14/01/2019.
+//  Copyright © 2019 윤성호. All rights reserved.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+    
+    // page2로 오는 이정표 메소드
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue){        
+    }
+}
